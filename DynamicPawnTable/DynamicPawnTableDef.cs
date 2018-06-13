faddbc9f8da50cbbde305df33b78a1f32101e201
Fluffy <fluffy.l2032@gmail.com>
2018-06-13 21:58:03 +0200

    initial commit

---
+++ DynamicPawnTable/DynamicPawnTableDef.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using RimWorld;
+using Verse;
+
+namespace DynamicPawnTable
+{
+    public class DynamicPawnTableDef: PawnTableDef
+    {
+        private List<PawnColumnDef> intColumns;
+
+        public List<PawnColumnDef> AllColumnsListForReading => intColumns.NullOrEmpty()
+            ? new List<PawnColumnDef>()
+            : new List<PawnColumnDef>( intColumns );
+
+        public void Select( Func<PawnColumnDef, bool> validator )
+        {
+            columns = intColumns.Where( validator ).ToList();
+        }
+
+        public override void PostLoad()
+        {
+            base.PostLoad();
+            LongEventHandler.ExecuteWhenFinished(delegate
+            {
+                intColumns = new List<PawnColumnDef>( columns );
+            });
+        }
+    }
+}
